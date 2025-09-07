fb63844b02a46f627d4dce22e96349af1f7046a2
Lennektro-Offical <lennha@gmx.de>
2025-09-07 03:52:52 +0200

    2025-09-07 03:52:52

--- analog-photography.js
+++ analog-photography.js
@@ -1,22 +1,6 @@
-function waitForImage(imgElem) {
-    return new Promise(res => {
-        if (imgElem.complete) {
-            return res();
-        }
-        imgElem.onload = () => res();
-        imgElem.onerror = () => res();
-    });
-}
-
-async function placeImage(grow, imid, date, img, thumb) {
-  grow.insertAdjacentHTML("beforeend", `<a title="${date}" class="pic" href="${img}"><h1>Loading Image...</h1><img id="apgimg${imid}" loading="lazy" src="${thumb}" /></a>`);
-  waitForImage(document.getElementById("apgimg" + imid));
-  imid++;
-}
-
 fetch("./content/gallery.json")
   .then(res => res.json())
-  .then(data => {    
+  .then(data => {
     let crow = 1;
     let imid = 0;
     for(let i in data.images) {
@@ -38,7 +22,28 @@ fetch("./content/gallery.json")
         crow++;
         if(crow > 3) crow = 1;
 
-        placeImage(grow, imid, date, img, thumb);
+        let imghtm = `<a title="${date}" class="pic" href="${img}"><h1>Loading Image...</h1><img id="${imid}" src="${thumb}" /></a>`;
+        if(imid > 11) imghtm = `<a title="${date}" class="pic" href="${img}"><h1>Loading Image...</h1><img class="lazy" id="${imid}" data-src="${thumb}" /></a>`;
+        grow.insertAdjacentHTML("beforeend", imghtm);
+        imid++;   
       }
     }
-  });
+    
+    const lazyImages = document.querySelectorAll("img.lazy");
+
+    const lazyLoad = () => {
+      lazyImages.forEach((image) => {
+        if(image.getBoundingClientRect().top <= window.innerHeight && image.getBoundingClientRect().bottom >= 0 && getComputedStyle(image).display !== "none") {
+          let previmg = document.getElementById(image.id - 3);
+          if(!previmg.classList.contains("lazy")) {
+            image.src = image.dataset.src;
+            image.classList.remove("lazy");
+          }
+        }
+      });
+    };
+
+    document.addEventListener("scroll", lazyLoad);
+    window.addEventListener("resize", lazyLoad);
+    window.addEventListener("orientationchange", lazyLoad);  
+});
