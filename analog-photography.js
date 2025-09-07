46d55172a0c0fb4bf6bfe644dfe364403075ed23
Lennektro-Offical <lennha@gmx.de>
2025-09-07 02:38:55 +0200

    2025-09-07 02:38:55

--- analog-photography.js
+++ analog-photography.js
@@ -1,17 +1,44 @@
+function waitForImage(imgElem) {
+    return new Promise(res => {
+        if (imgElem.complete) {
+            return res();
+        }
+        imgElem.onload = () => res();
+        imgElem.onerror = () => res();
+    });
+}
+
+async function placeImage(grow, imid, date, img, thumb) {
+  grow.insertAdjacentHTML("beforeend", `<a title="${date}" class="pic" href="${img}"><h1>Loading Image...</h1><img id="apgimg${imid}" loading="lazy" src="${thumb}" /></a>`);
+  waitForImage(document.getElementById("apgimg" + imid));
+  imid++;
+}
+
 fetch("./content/gallery.json")
   .then(res => res.json())
-  .then(data => {
+  .then(data => {    
     let crow = 1;
+    let imid = 0;
     for(let i in data.images) {
-      //let date = data.images[i].d;
+      let date = data.images[i].d;
       for(let j in data.images[i].i) {
-        let img = data.images[i].i[j];
+        let imgEntry = data.images[i].i[j];
+        let img = null;
+        let thumb = null;
+        if(typeof imgEntry === "string") {
+          thumb = imgEntry;
+          img = imgEntry;        
+        } else {
+          thumb = imgEntry[0];
+          img = imgEntry[1];
+        }
+
         let grow = document.getElementById("grow" + crow);
 
         crow++;
         if(crow > 3) crow = 1;
 
-        grow.insertAdjacentHTML("beforeend", `<a class="pic" href="${img}"><h1>Loading Image...</h1><img loading="lazy" src="${img}" /></a>`);
+        placeImage(grow, imid, date, img, thumb);
       }
     }
   });
